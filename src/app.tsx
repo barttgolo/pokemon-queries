372a3a7d26ee59175086189359c4135a5aaae974
barttgolo <barttgolon@gmail.com>
2024-11-13 11:14:13 +0100

    rollback

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,9 @@
-// import { Pokemon } from "@/components/pokemon";
+import { Pokemon } from "@/components/pokemon";
 
 export const App = () => {
   return (
     <div className="min-h-screen bg-slate-100 flex flex-col gap-4 items-center justify-center">
-      {/* <Pokemon /> */}
-      <a href="jutro://Tabs/HomeTabs" target="_blank">
-        Deeplink: "jutro://Tabs/HomeTabs"
-      </a>
-
-      <a
-        href="intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro.jutro;end"
-        target="_blank"
-      >
-        Deeplink:
-        "intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro.jutro;end"
-      </a>
-
-      <a
-        href="intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro;end"
-        target="_blank"
-      >
-        Deeplink: "intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro;end"
-      </a>
+      <Pokemon />
     </div>
   );
 };
