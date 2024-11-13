b938f31db45a7e937196f92f4e87f471d087e516
barttgolo <barttgolon@gmail.com>
2024-11-13 11:03:46 +0100

    deeplink test

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,27 @@
-import { Pokemon } from "@/components/pokemon";
+// import { Pokemon } from "@/components/pokemon";
 
 export const App = () => {
   return (
-    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
-      <Pokemon />
+    <div className="min-h-screen bg-slate-100 flex flex-col gap-4 items-center justify-center">
+      {/* <Pokemon /> */}
+      <a href="jutro://Tabs/HomeTabs" target="_blank">
+        Deeplink: "jutro://Tabs/HomeTabs"
+      </a>
+
+      <a
+        href="intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro.jutro;end"
+        target="_blank"
+      >
+        Deeplink:
+        "intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro.jutro;end"
+      </a>
+
+      <a
+        href="intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro;end"
+        target="_blank"
+      >
+        Deeplink: "intent://Tabs/HomeTabs;scheme=jutro;package=com.jutro;end"
+      </a>
     </div>
   );
 };
